b9fd98ea07e50edf291d66c95da648dd84f2a509
shane-221 <shane.mathew98@outlook.com>
2026-01-20 15:48:18 +0000

    Day 55 One task left

--- 08_Intermediate Python Tooling,. Basic Web Serving & Professional Foundations/55_Day_55-Advanced_Decorators/Task_1.py
+++ 08_Intermediate Python Tooling,. Basic Web Serving & Professional Foundations/55_Day_55-Advanced_Decorators/Task_1.py
@@ -6,7 +6,7 @@ from flask import Flask
 app= Flask (__name__)
 
 @app.route("/")
-def index():
+def indexx():
     return "Hello World"
 
 @app.route("/<name>")
---
+++ 08_Intermediate Python Tooling,. Basic Web Serving & Professional Foundations/55_Day_55-Advanced_Decorators/Task_2_Args_Kwargs.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+## Decorator Function
+def bold(function):
+    def wrapper():
+        print(">>> bold decorator ran")
+        output = function()
+        return f"<b>{output}</b><html>"
+    return wrapper
+
+@app.route("/")
+@bold
+def home():
+    return "Hello World"
+
+@app.route("/<name>")
+def names(name):
+    return f"Hello {name}"
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True, use_reloader=False)
