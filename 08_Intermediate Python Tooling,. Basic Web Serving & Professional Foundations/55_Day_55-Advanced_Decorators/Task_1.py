f207363d61c562b4cbaf028c1d7c6a34498b0840
shane-221 <shane.mathew98@outlook.com>
2026-01-20 14:16:31 +0000

    Task 1 for day 55 and Adjstments for Day 54

---
+++ 08_Intermediate Python Tooling,. Basic Web Serving & Professional Foundations/55_Day_55-Advanced_Decorators/Task_1.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+
+
+
+app= Flask (__name__)
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+@app.route("/<name>")
+def home(name):
+    return f"Hello {name}"
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
