e0e48253423813d0dfe95bdca1599963deafa1ce
shane-221 <shane.mathew98@outlook.com>
2026-01-26 12:29:25 +0000

    Day 71 - additon of a Procfile

--- 08_Intermediate Python Tooling,. Basic Web Serving & Professional Foundations/71_Day_71_Github_Version_Control&Deploy_Webpage/main.py
+++ 08_Intermediate Python Tooling,. Basic Web Serving & Professional Foundations/71_Day_71_Github_Version_Control&Deploy_Webpage/main.py
@@ -10,8 +10,7 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-# Optional: add contact me email functionality (Day 60)
-# import smtplib
+import smtplib
 
 
 '''
