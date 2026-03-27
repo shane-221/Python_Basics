191c135a63ecd2e4c39b8d6864ba48fb67991884
shane-221 <shane.mathew98@outlook.com>
2026-03-27 17:18:52 +0000

    Day 57  Task 1

---
+++ 08_Building_Websites/57_Templating_with_Jinga/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main_page():
+    random_number = random.randint(1, 10)
+    return render_template("index.html", num=random_number)
+
+
+if __name__ =="__main__":
+    app.run(debug= True )
