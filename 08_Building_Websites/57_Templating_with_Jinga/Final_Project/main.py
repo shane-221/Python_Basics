7c5a312a7c0585dc5a85796919e3f30ddbaa0076
shane-221 <shane.mathew98@outlook.com>
2026-03-30 17:42:23 +0100

    improved content

--- 08_Building_Websites/57_Templating_with_Jinga/Final_Project/main.py
+++ 08_Building_Websites/57_Templating_with_Jinga/Final_Project/main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 import requests 
 
+blog_url= "https://api.npoint.io/c790b4d5cab58020d391"
+response= requests.get(blog_url)
+all_posts= response.json()
+
 
 app = Flask(__name__)
 
@@ -8,17 +12,11 @@ app = Flask(__name__)
 ## Blog sections 
 @app.route("/")
 def blog():
-    blog_url= "https://api.npoint.io/c790b4d5cab58020d391"
-    response= requests.get(blog_url)
-    all_posts= response.json()
     return render_template("post.html", posts= all_posts)
 
 ## Read route 
 @app.route("/post/<int:id>")
 def id_page(id):
-    blog_url= "https://api.npoint.io/c790b4d5cab58020d391"
-    response= requests.get(blog_url)
-    all_posts= response.json()
     post = [ post for post in all_posts  if post["id"]==id][0]
     print(post)
     return render_template("read_page.html", posts = post)
